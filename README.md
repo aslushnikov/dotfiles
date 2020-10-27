1e69f322039f6ff450ca32eaaa521725eeff09ef
Andrey Lushnikov <lushnikov@chromium.org>
2020-10-26 19:48:16 -0700

    feat: add bootstrap.sh to simplify new setups

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
+
 # Prerequisites
 - [Vim distinguished color theme](https://github.com/Lokaltog/vim-distinguished)
 - [reattach-to-user-namespace](https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard) to fix pbcopy/pbpaste on OS X in tmux. NB: under iTerm2, a checkbox "Allow clipboard access to terminal apps" should be checked.
 - [diff-so-fancy](https://github.com/so-fancy/diff-so-fancy) to see nice git diffs in terminal
 - [fzf](https://github.com/junegunn/fzf) for fuzzy-search files in terminal
 - [git bash completions](https://github.com/git/git/blob/master/contrib/completion/git-completion.bash) so that bash would complete branchnames etc on Tab.
+
+> **NOTE**: run `./bootstrap.sh` to link dotfiles, install fzf, diff-so-fancy and vim colorscheme.
 
 # Fresh MacOS Setup
 
---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+set +x
+
+trap "cd $(pwd -P);" INT TERM EXIT
+cd "$(dirname "$0")"
+
+# Link all files to $HOME
+rm -rf "$HOME/.bashrc"
+rm -rf "$HOME/.tmux.conf"
+rm -rf "$HOME/.gitconfig"
+rm -rf "$HOME/.vimrc"
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+ln -s "$PWD/bashrc" "$HOME/.bashrc"
+ln -s "$PWD/vimrc" "$HOME/.vimrc"
+
+# install vim colorscheme
+mkdir -p "$HOME/.vim/colors"
+wget --directory-prefix="$HOME/.vim/colors" https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors/distinguished.vim
+
+# install diff-so-fancy
+mkdir -p "$HOME/prog"
+git clone https://github.com/so-fancy/diff-so-fancy "$HOME/prog/diff-so-fancy"
+sudo ln -s "$HOME/prog/diff-so-fancy/diff-so-fancy" /usr/local/bin/diff-so-fancy
+
+# install FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+"$HOME/.fzf/install"
+
+# install bash completions
+wget --directory-prefix="$HOME" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
